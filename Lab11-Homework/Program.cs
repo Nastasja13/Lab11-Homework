7dba380a174fa06d554efba4787f8e67049d8deb
Anastacia Golovaneva <nastasja13.com@gmail.com>
2021-12-12 17:47:16 +0300

    Добавьте файлы проекта.

---
+++ Lab11-Homework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_for_Lab11_Homework;
+
+namespace Lab11_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Разработать структуру для решения линейного уравнения 0=kx+b.              *  
+             *  Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+             *  Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. 
+             *  Осуществить использование экземпляра в программе.*/
+
+            Equation equation = new Equation();
+
+
+            Console.WriteLine("Введите значения для решения линейного уравнения 0=kx+b.");
+            Console.Write("Значение k: ");
+            equation.K = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Значение b: ");
+            equation.B = Convert.ToInt32(Console.ReadLine());           
+
+            //Метод Root для поиска x = (0 - b) / k
+            Root();
+            void Root()
+            {
+                double x = (0 - equation.B) / equation.K;
+                Console.WriteLine("Значение x: {0:f2}", x);
+            }
+
+            Console.ReadKey();
+        }
+
+        
+
+
+
+    }
+}
+
---
+++ StructEquation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructEquation
+{
+    public struct Equation
+    {
+        double k;
+        double b;
+
+        public double K
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    k = value;
+                }
+                else
+                {
+                    Console.WriteLine("Введите положительное число");
+                }
+            }
+            get
+            {
+                return k;
+            }
+        }
+
+        public double B
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    b = value;
+                }
+                else
+                {
+                    Console.WriteLine("Введите положительное число");
+                }
+            }
+            get
+            {
+                return b;
+            }
+        }
+
+
+    }
+}
